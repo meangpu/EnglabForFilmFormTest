a9ea23d2846d092f0ed6d73a9735b5544764893e
meangpu <51825872+meangpu@users.noreply.github.com>
2024-03-20 20:02:01 +0700

    add data container

--- EnglabTest/src/App.tsx
+++ EnglabTest/src/App.tsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <header>
+        <h1>Choose your test</h1>
+      </header>
+
+      <section class='image-grid'>
+        <a href='https://forms.gle/1FwLbUx7N3vivF9HA' target='_blank'>
+          <div class='grid-item'>
+            <img src='Image/1.png' width='512' height='512' alt='unit 1' />
+            <p>Unit1</p>
+          </div>
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+
+        <a href='https://forms.gle/JwuMk6UfyjcuPiu96' target='_blank'>
+          <div class='grid-item'>
+            <img src='Image/2.png' width='512' height='512' alt='unit 1' />
+            <p>Unit2</p>
+          </div>
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+        <a href='https://forms.gle/6C2swqWTMHVJbPLeA' target='_blank'>
+          <div class='grid-item'>
+            <img src='Image/3.png' width='512' height='512' alt='unit 1' />
+            <p>Unit3</p>
+          </div>
+        </a>
+
+        <a href='https://forms.gle/bwqV2oB6iVUb8joq9' target='_blank'>
+          <div class='grid-item'>
+            <img src='Image/4.png' width='512' height='512' alt='unit 1' />
+            <p>Unit4</p>
+          </div>
+        </a>
+
+        <a href='https://forms.gle/vcUz4EkYYPgBBW4a8' target='_blank'>
+          <div class='grid-item'>
+            <img src='Image/5.png' width='512' height='512' alt='unit 1' />
+            <p>Unit5</p>
+          </div>
+        </a>
+
+        <a href='https://forms.gle/4apMbcVEXA365vGQ8' target='_blank'>
+          <div class='grid-item'>
+            <img src='Image/6.png' width='512' height='512' alt='unit 1' />
+            <p>Unit6</p>
+          </div>
+        </a>
+
+        <a href='https://forms.gle/fe5q1RTHQ4HGznVCA' target='_blank'>
+          <div class='grid-item'>
+            <img src='Image/7.png' width='512' height='512' alt='unit 1' />
+            <p>Unit7</p>
+          </div>
+        </a>
+
+        <a href='https://forms.gle/3Yvd4bwkVZGEmC2CA' target='_blank'>
+          <div class='grid-item'>
+            <img src='Image/8.png' width='512' height='512' alt='unit 1' />
+            <p>Unit8</p>
+          </div>
+        </a>
+
+        <a href='https://forms.gle/YueAqH5Y2fKmcBnVA' target='_blank'>
+          <div class='grid-item'>
+            <img src='Image/9.png' width='512' height='512' alt='unit 1' />
+            <p>Unit9</p>
+          </div>
+        </a>
+
+        <a href='https://forms.gle/Q5fESYJNAEA2WkMq6' target='_blank'>
+          <div class='grid-item'>
+            <img src='Image/10.png' width='512' height='512' alt='unit 1' />
+            <p>Unit10</p>
+          </div>
+        </a>
+
+        <a href='https://forms.gle/ccNvJc2U7qxR4jGA9' target='_blank'>
+          <div class='grid-item'>
+            <img src='Image/final.png' width='512' height='512' alt='unit 1' />
+            <p>finalTest</p>
+          </div>
+        </a>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ EnglabTest/src/DataContainer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DataContainer = () => {
+  return <div>DataContainer</div>;
+};
+
+export default DataContainer;
