82c06bb7d76649e163e96338abce4f957c4b19d4
meangpu <51825872+meangpu@users.noreply.github.com>
2024-03-21 01:07:47 +0700

    update docs

--- EnglabTest/vite.config.ts
+++ EnglabTest/vite.config.ts
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "https://github.com/meangpu/EnglabForFilmFormTest/",
+  build: {
+    outDir: "../docs",
+  },
 });
