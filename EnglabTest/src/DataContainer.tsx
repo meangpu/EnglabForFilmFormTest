74ed1038a8db19886c299e5a44142756c8e75285
meangpu <51825872+meangpu@users.noreply.github.com>
2024-03-20 20:06:58 +0700

    Update DataContainer.tsx

--- EnglabTest/src/DataContainer.tsx
+++ EnglabTest/src/DataContainer.tsx
@@ -1,7 +1,33 @@
-import React from "react";
-
-const DataContainer = () => {
-  return <div>DataContainer</div>;
+const DataContainer = (props: {
+  formUrl: string;
+  imageFile: string;
+  unit: string;
+  previewImage: string;
+  title: string;
+}) => {
+  return (
+    <div>
+      <a href={props.formUrl} target='_blank'>
+        <div className='grid-item'>
+          <img
+            src={props.imageFile}
+            width='512'
+            height='512'
+            alt={props.unit}
+          />
+          <img
+            src={props.previewImage}
+            width='512'
+            height='512'
+            alt={props.unit}
+          />
+          <p>
+            `${props.unit} ${props.title}`
+          </p>
+        </div>
+      </a>
+    </div>
+  );
 };
 
 export default DataContainer;
